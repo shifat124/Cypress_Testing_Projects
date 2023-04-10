3061cb66faf8b48d6700789e447d7a90c9ff6fcf
shifat124 <shifatlaskor779@gmail.com>
2023-04-10 16:48:17 +0600

    chiltabs project added

---
+++ cypress/e2e/ChildTabs.cy.js
@@ -0,0 +1,26 @@
+describe('Child Tabs Test', () => {
+    it('child tabs test - approach 1', () => {
+
+        cy.visit("https://the-internet.herokuapp.com/windows")
+        cy.get('.example >a').invoke('removeAttr', 'target').click()
+        cy.url().should('include', 'https://the-internet.herokuapp.com/windows/new')
+        cy.wait(5000)
+        cy.go('back') 
+     
+    })
+
+    it('child tabs test - approach 2', () => {
+
+        cy.visit("https://the-internet.herokuapp.com/windows")
+        cy.get('.example >a').then((e)=>{
+            let url = e.prop('href')
+            cy.visit(url)
+        })
+        cy.url().should('include', 'https://the-internet.herokuapp.com/windows/new')
+        cy.wait(5000)
+        cy.go('back') 
+     
+    })
+
+   
+  })
